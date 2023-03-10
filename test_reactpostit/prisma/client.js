7bf1cfa3243543010f668cdf10f43627c2bb8b8a
Anita Chu <c8anita@gmail.com>
2023-03-10 17:34:03 -0500

    Fully integrated web application with nextjs, prisma and tailwindCSS

---
+++ test_reactpostit/prisma/client.js
@@ -0,0 +1,10 @@
+import { PrismaClient } from '@prisma/client'
+
+// if it already exits: check on global env if prisma exists 
+// /// --> no new client
+// creating new prisma client
+const client = globalthis.prisma || new PrismaClient()
+// checking if its in production or not
+if (process.env.NODE_ENV !== "production") globalThis.prisma = client
+
+export default client
