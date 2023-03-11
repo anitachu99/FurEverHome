7118327f11b33e85ff39c9300bb3c4f877cc0bf3
Anita Chu <c8anita@gmail.com>
2023-03-10 20:28:01 -0500

    Get server session and authorization

--- test_reactpostit/app/auth/Login.tsx
+++ test_reactpostit/app/auth/Login.tsx
@@ -5,7 +5,7 @@ import {signIn} from 'next-auth/react';
 export default function Login() {
     return(
         <li className='list-none'>
-            <button onClick={() => signIn()} className='text-sm '>
+            <button onClick={() => signIn()} className='signin-button'>
                 Sign In
             </button>
         </li>
--- test_reactpostit/app/auth/Nav.tsx
+++ test_reactpostit/app/auth/Nav.tsx
@@ -2,14 +2,18 @@
 // get users straight from server
 import Link from "next/link";
 import Login from './Login';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '../../pages/api/auth/[...nextauth]';
 
 export default async function Nav() {
+    const session = await getServerSession(authOptions);
+    console.log(session);
     return( 
-        <nav className="flex justify-between items-center py-8">
+        <nav className="nav-panel">
             <Link href={'/'}>
-                <h1 className="font-bold text-lg">Send it.</h1>
+                <h1 className="sendit-link">Send it</h1>
             </Link>
-            <ul className="flex items-center gap-6">
+            <ul className="signIn">
                 <Login />
             </ul>
         </nav>
