12486d93a63f0795f498d4122d8bf0dfce070b06
Anita Chu <c8anita@gmail.com>
2023-03-10 20:11:16 -0500

    Fixed signin client and added styling

--- test_reactpostit/prisma/client.js
+++ test_reactpostit/prisma/client.js
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client'
 // if it already exits: check on global env if prisma exists 
 // /// --> no new client
 // creating new prisma client
-const client = globalthis.prisma || new PrismaClient()
+const client = globalThis.prisma || new PrismaClient()
 // checking if its in production or not
 if (process.env.NODE_ENV !== "production") globalThis.prisma = client
 
